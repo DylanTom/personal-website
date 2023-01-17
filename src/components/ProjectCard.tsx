3bd6a17705cd12b11c9eeb4716dafb455be6c0c1
Dylan Tom <dt425@cornell.edu>
2023-01-17 09:50:22 -0500

    Fixed hover setting for projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -20,6 +20,26 @@ function ProjectCard(props: TProject) {
 		isHover(false);
 	}
 
+	console.log(hover)
+
+	if (hover) {
+		return (
+			<div className={"relative flex flex-col shadow-md hover:shadow-xl hover: cursor-pointer justify-center items-center m-6 border-2 rounded-xl"} onMouseEnter={e => showOptions(e)} onMouseLeave={e => hideOptions(e)}>
+				<div className="flex flex-row space-x-20 justify-center items-center p-6 bg-[#f5f5f5] w-full h-full">
+					<div className="flex">
+						<a href={props.link} target="_blank">
+							<img src={github} width="48px" height="48px" />
+						</a>
+					</div>
+					<div className="flex">
+						<img src={seeMore} width="48px" height="48px" onClick={() => setIsOpen(true)} />
+						<Modal open={isOpen} setIsOpen={setIsOpen} data={props} />
+					</div>
+				</div>
+			</div>
+		)
+	}
+
 	return (
 		<div className="relative flex flex-col shadow-md hover:shadow-xl hover: cursor-pointer justify-center items-center m-6 border-2 rounded-xl" onMouseEnter={e => showOptions(e)} onMouseLeave={e => hideOptions(e)}>
 			<img src={props.img} className="w-full h-36 md:h-48" />
@@ -35,19 +55,37 @@ function ProjectCard(props: TProject) {
 					})}
 				</div>
 			</div>
-			<div className={hover ? "flex flex-row space-x-20 justify-center items-center p-6 bg-[#f5f5f5] w-full h-full inset-0" : "hidden"}>
-					<div className="flex">
-						<a href={props.link} target="_blank" className="hover:shadow-md">
-							<img src={github} width="48px" height="48px"/>
-						</a>
-					</div>
-					<div className="flex">
-						<img src={seeMore} width="48px" height="48px" onClick={() => setIsOpen(true)} className="hover:shadow-md"/>
-						<Modal open={isOpen} setIsOpen={setIsOpen} data={props}/>
-					</div>
-				</div>
 		</div>
 	);
+
+	// return (
+	// 	<div className="relative flex flex-col shadow-md hover:shadow-xl hover: cursor-pointer justify-center items-center m-6 border-2 rounded-xl" onMouseEnter={e => showOptions(e)} onMouseLeave={e => hideOptions(e)}>
+	// 		<img src={props.img} className="w-full h-36 md:h-48" />
+	// 		<div className="w-full p-4 mx-6">
+	// 			<div className="text-xl md:text-2xl mb-2 md:mb-3 font-semibold2 text-left">
+	// 				{props.title}
+	// 			</div>
+	// 			<div className="flex flex-wrap gap-2 flex-row items-start justify-start text-xs md:text-sm">
+	// 				{props.stack.sort().map(item => {
+	// 					return <span className="inline-block px-2 py-1 border-2 border-stone-900 rounded-md">
+	// 						{item}
+	// 					</span>
+	// 				})}
+	// 			</div>
+	// 		</div>
+	// 		<div className={hover ? "flex flex-row space-x-20 justify-center items-center p-6 bg-[#f5f5f5] w-full h-full inset-0 object-cover" : "hidden"}>
+	// 				<div className="flex">
+	// 					<a href={props.link} target="_blank" className="hover:shadow-md">
+	// 						<img src={github} width="48px" height="48px"/>
+	// 					</a>
+	// 				</div>
+	// 				<div className="flex">
+	// 					<img src={seeMore} width="48px" height="48px" onClick={() => setIsOpen(true)} className="hover:shadow-md"/>
+	// 					<Modal open={isOpen} setIsOpen={setIsOpen} data={props}/>
+	// 				</div>
+	// 			</div>
+	// 	</div>
+	// );
 }
 
 export default ProjectCard;
